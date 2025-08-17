6bf879bb8272f31870fc80202bb7e62a75199d06
Md Saiful <saifulislam66info@gmail.com>
2025-08-18 02:26:28 +0600

    complite task 2

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-function tasl1(str) {
+function task1(str) {
   return str.split("").reverse().join("");
 
   console.log(result);
 }
 
-console.log(tasl1("hello"));
+console.log(task1("hello"));
+
+function task2(str) {
+  let mainStr = str;
+  let reverseStr = str.split("").reverse().join("");
+  if (mainStr === reverseStr) {
+    return "The string is a palindrome";
+  }
+  {
+    return "The string is not a palindrome";
+  }
+}
+
+console.log(task2("hello"));
