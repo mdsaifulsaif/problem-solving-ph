3031e235c7efc813f23dc295fc24dfc98902b53e
Md Saiful <saifulislam66info@gmail.com>
2025-08-19 14:25:33 +0600

    solve 10 problem

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function task1(str) {
 }
 // takst number 2
 
-// console.log(task1("hello"));
+console.log(task1("hello"));
 
 function task2(str) {
   const vowels = "aeiouAEIOU";
@@ -16,8 +16,7 @@ function task2(str) {
   }
   return count;
 }
-
-// console.log(task2("programmingaoio"));
+console.log(task2("programmingaoio"));
 
 // takst number 3
 function task3(str) {
@@ -30,7 +29,70 @@ function task3(str) {
     return "The string is not a palindrome";
   }
 }
-
 console.log(task3("hello"));
 
 // takst number 4
+function task4(arr) {
+  return Math.max(...arr);
+}
+
+console.log(task4([5, 1, 9, 3]));
+
+// task number 5
+
+function task5(arr) {
+  return [...new Set(arr)];
+}
+
+// Example
+console.log(task5([1, 2, 2, 3, 4, 4]));
+
+// task number 6
+function task6(arr) {
+  return arr.reduce((sum, num) => sum + num, 0);
+}
+
+console.log(task6([1, 2, 3, 4]));
+
+// task number 7
+function task7(arr) {
+  return arr.filter((num) => num % 2 === 0);
+}
+
+console.log(task7([1, 2, 3, 4, 5, 6]));
+
+// task number 8
+function task8(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+console.log(task8("hello world"));
+
+// task number 9
+function task9(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(task9(5));
+// task number 10
+function task10() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+task10();
