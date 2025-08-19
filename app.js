cca82fcee048293e82c7091d5a92a67370005371
Md Saiful <saifulislam66info@gmail.com>
2025-08-19 13:40:51 +0600

    solve problem 2

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+// takst number 1
 function task1(str) {
   return str.split("").reverse().join("");
 
   console.log(result);
 }
+// takst number 2
 
-console.log(task1("hello"));
+// console.log(task1("hello"));
 
 function task2(str) {
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+  for (let char of str) {
+    if (vowels.includes(char)) count++;
+  }
+  return count;
+}
+
+// console.log(task2("programmingaoio"));
+
+// takst number 3
+function task3(str) {
   let mainStr = str;
   let reverseStr = str.split("").reverse().join("");
   if (mainStr === reverseStr) {
@@ -17,4 +31,6 @@ function task2(str) {
   }
 }
 
-console.log(task2("hello"));
+console.log(task3("hello"));
+
+// takst number 4
